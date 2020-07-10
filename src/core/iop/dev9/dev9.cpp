e6f15b7d4bf4ca0fd32acffc47ef8370ea334fc4
Ziemas <ziemas@ziemas.se>
2020-07-11 00:02:05 +0200

    Just filling in some regs

--- src/core/iop/dev9/dev9.cpp
+++ src/core/iop/dev9/dev9.cpp
@@ -3,38 +3,138 @@
 
 uint8_t DEV9::read8(uint32_t address)
 {
+    if (address >= SMAP_REGBASE && address <= FLASH_REGBASE)
+    {
+      //printf("[DEV9] Read16 unrecognized SMAP reg\n");
+      // SMAP
+      return 0;
+    }
+
+    switch (address)
+    {
+        case SPD_REG(SPD_R_PIO_DIR):
+        {
+            printf("[DEV9] Read8 PIO_DIR: %04x\n", 0);
+            return 0;
+        }
+        case SPD_REG(SPD_R_PIO_DATA):
+        {
+            printf("[DEV9] Read8 PIO_DATA: %04x\n", 0);
+            return 0;
+        }
+    }
     printf("Unrecognized DEV9 read8 from $%08x\n", address);
     return 0;
 }
 
 uint16_t DEV9::read16(uint32_t address)
 {
+    if (address >= SMAP_REGBASE && address <= FLASH_REGBASE)
+    {
+      //printf("[DEV9] Read16 unrecognized SMAP reg\n");
+      // SMAP
+      return 0;
+    }
     switch (address)
     {
         case 0x1F80146E:
-            return DEV9_IDENT;
+        {
+            printf("[DEV9] Read ident magic\n");
+            return DEV9_IDENT; // magic number
+        }
+        case SPD_REG(SPD_R_REV_1):
+        {
+            printf("[DEV9] Read magic number the second coming\n");
+            return 0x0011; // magic number
+        }
+        case SPD_REG(SPD_R_REV_3):
+        {
+            printf("[DEV9] Read caps\n");
+            return SPD_CAPS_SMAP; // lets lie a litle
+        }
+        case SPD_REG(SPD_R_INTR_STAT):
+        {
+            printf("[DEV9] Read INTR_STAT: %04x\n", irq_stat);
+            return irq_stat;
+        }
+        case SPD_REG(SPD_R_INTR_MASK):
+        {
+            printf("[DEV9] Read INTR_MASK: %04x\n", irq_mask);
+            return irq_mask;
+        }
     }
-    printf("Unrecognized DEV9 read16 from $%08x\n", address);
+
+    printf("[DEV9] Unrecognized read16 from $%08x\n", address);
     return 0;
 }
 
 uint32_t DEV9::read32(uint32_t address)
 {
-    printf("Unrecognized DEV9 read32 from $%08x\n", address);
+    if (address >= SMAP_REGBASE && address <= FLASH_REGBASE)
+    {
+      //printf("[DEV9] Read16 unrecognized SMAP reg\n");
+      // SMAP
+      return 0;
+    }
+
+    printf("[DEV9] Unrecognized read32 from $%08x\n", address);
     return 0;
 }
 
 void DEV9::write8(uint32_t address, uint8_t value)
 {
-    printf("Unrecognized DEV9 write8 to $%08x of $%01x\n", address, value);
+    if (address >= SMAP_REGBASE && address <= FLASH_REGBASE)
+    {
+      printf("[DEV9] Unrecognized SMAP write8 to $%04x of %01x\n", address, value);
+      // SMAP
+      return;
+    }
+
+    switch (address)
+    {
+        case SPD_REG(SPD_R_PIO_DIR):
+        {
+            printf("[DEV9] Write8 PIO_DIR: %04x\n", value);
+            return;
+        }
+        case SPD_REG(SPD_R_PIO_DATA):
+        {
+            printf("[DEV9] Write8 PIO_DATA: %04x\n", value);
+            return;
+        }
+    }
+    printf("[DEV0] Unrecognized DEV9 write8 to $%08x of $%01x\n", address, value);
 }
 
 void DEV9::write16(uint32_t address, uint16_t value)
 {
-    printf("Unrecognized DEV9 write16 to $%08x of $%02x\n", address, value);
+    if (address >= SMAP_REGBASE && address <= FLASH_REGBASE)
+    {
+      printf("[DEV9] Unrecognized SMAP write16 to $%04x of %02x\n", address, value);
+      // SMAP
+      return;
+    }
+
+    switch (address)
+    {
+        case SPD_REG(SPD_R_INTR_MASK):
+        {
+            printf("[DEV9] Write16 INTR_MASK: %04x\n", value);
+            irq_mask = value;
+            return;
+        }
+    }
+
+    printf("[DEV9] Unrecognized write16 to $%08x of $%02x\n", address, value);
 }
 
 void DEV9::write32(uint32_t address, uint32_t value)
 {
-    printf("Unrecognized DEV9 write32 to $%08x of $%04x\n", address, value);
+    if (address >= SMAP_REGBASE && address <= FLASH_REGBASE)
+    {
+      printf("[DEV9] Unrecognized SMAP write32 to $%04x of %04x\n", address, value);
+      // SMAP
+      return;
+    }
+    printf("[DEV9] Unrecognized DEV9 write32 to $%08x of $%04x\n", address, value);
 }
--- src/core/iop/dev9/dev9.hpp
+++ src/core/iop/dev9/dev9.hpp
@@ -5,17 +5,46 @@
 
 #define DEV9_IDENT 0x32
 
+#define SPD_REGBASE 0x10000000
+#define SMAP_REGBASE (SPD_REGBASE + 0x100)
+#define FLASH_REGBASE 0x10004800
+
+#define SPD_REG(offset) (SPD_REGBASE + (offset))
+//#define SPD_REG8(offset) (uint16_t)(SPD_REGBASE + (offset))
+//#define SPD_REG16(offset) (uint16_t)(SPD_REGBASE + (offset))
+//#define SPD_REG32(offset) (uint16_t)(SPD_REGBASE + (offset))
+
+#define SPD_CAPS_SMAP (1 << 0)
+#define SPD_CAPS_ATA (1 << 1)
+#define SPD_CAPS_UART (1 << 3)
+#define SPD_CAPS_DVR (1 << 4)
+#define SPD_CAPS_FLASH (1 << 5)
+
+#define SPD_R_DMA_CTRL 0x24
+#define SPD_R_INTR_STAT 0x28
+#define SPD_R_INTR_MASK 0x2a
+#define SPD_R_PIO_DIR 0x2c
+#define SPD_R_PIO_DATA 0x2e
+
+#define SPD_R_REV 0x00
+#define SPD_R_REV_1 0x02
+#define SPD_R_REV_3 0x04
+#define SPD_R_REV_8 0x0e
+
 class DEV9
 {
     private:
-    public:
-    uint8_t read8(uint32_t address);
-    uint16_t read16(uint32_t address);
-    uint32_t read32(uint32_t address);
+        uint16_t irq_stat = 0;
+        uint16_t irq_mask = 0;
 
-    void write8(uint32_t address, uint8_t value);
-    void write16(uint32_t address, uint16_t value);
-    void write32(uint32_t address, uint32_t value);
+    public:
+        uint8_t read8(uint32_t address);
+        uint16_t read16(uint32_t address);
+        uint32_t read32(uint32_t address);
+
+        void write8(uint32_t address, uint8_t value);
+        void write16(uint32_t address, uint16_t value);
+        void write32(uint32_t address, uint32_t value);
 };
 
 
